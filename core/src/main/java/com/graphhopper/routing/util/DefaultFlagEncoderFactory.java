836232f201ac8ccf0dbfea7ac07825e863b626a9
Bettina Auschra <ausb@hrz.tu-chemnitz.de>
2018-06-08 21:42:15 +0200

    added indoor flag encoder to flag encoder factory

--- core/src/main/java/com/graphhopper/routing/util/DefaultFlagEncoderFactory.java
+++ core/src/main/java/com/graphhopper/routing/util/DefaultFlagEncoderFactory.java
@@ -57,6 +57,9 @@ public class DefaultFlagEncoderFactory implements FlagEncoderFactory {
         if (name.equals(MOTORCYCLE))
             return new MotorcycleFlagEncoder(configuration);
 
+        if (name.equals(INDOOR))
+            return new IndoorFlagEncoder(configuration);
+
         throw new IllegalArgumentException("entry in encoder list not supported " + name);
     }
 }
--- core/src/main/java/com/graphhopper/routing/util/FlagEncoderFactory.java
+++ core/src/main/java/com/graphhopper/routing/util/FlagEncoderFactory.java
@@ -33,6 +33,7 @@ public interface FlagEncoderFactory {
     final String HIKE = "hike";
     final String MOTORCYCLE = "motorcycle";
     final String GENERIC = "generic";
+    final String INDOOR = "indoor";
     final FlagEncoderFactory DEFAULT = new DefaultFlagEncoderFactory();
 
     FlagEncoder createFlagEncoder(String name, PMap configuration);
