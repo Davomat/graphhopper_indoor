adcdc44ffc26266d8a078f6f2e221c12682b6ec7
Bettina Auschra <ausb@hrz.tu-chemnitz.de>
2018-06-09 14:32:15 +0200

    added level index to store the levels of the nodes of an indoor way, added indoor flag encoder

---
+++ core/src/main/java/com/graphhopper/routing/util/IndoorFlagEncoder.java
@@ -0,0 +1,253 @@
+/*
+ *  Licensed to GraphHopper GmbH under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for
+ *  additional information regarding copyright ownership.
+ *
+ *  GraphHopper GmbH licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.graphhopper.routing.util;
+
+import com.graphhopper.reader.ReaderRelation;
+import com.graphhopper.reader.ReaderWay;
+import com.graphhopper.routing.weighting.PriorityWeighting;
+import com.graphhopper.util.EdgeIteratorState;
+import com.graphhopper.util.PMap;
+
+import java.util.*;
+
+import static com.graphhopper.routing.util.PriorityCode.*;
+
+
+public class IndoorFlagEncoder extends AbstractFlagEncoder {
+    static final int SLOW_SPEED = 2;
+    static final int MEAN_SPEED = 5;
+    final Set<String> safeHighwayTags = new HashSet<String>();
+    final Set<String> allowedHighwayTags = new HashSet<String>();
+    final Set<String> avoidHighwayTags = new HashSet<String>();
+    //final LevelIndex levelIndex = new LevelIndex("test");
+
+    //levels in the building
+    final Set<String> allLevels = new HashSet<String>();
+
+    // convert network tag of hiking routes into a way route code
+    private EncodedValue priorityWayEncoder;
+    private EncodedValue relationCodeEncoder;
+
+
+    /**
+     * Should be only instantiated via EncodingManager
+     */
+    public IndoorFlagEncoder() {
+        this(4, 1);
+    }
+
+    public IndoorFlagEncoder(PMap properties) {
+        this((int) properties.getLong("speedBits", 4),
+                properties.getDouble("speedFactor", 1));
+        this.properties = properties;
+        this.setBlockFords(properties.getBool("block_fords", true));
+    }
+
+
+    public IndoorFlagEncoder(int speedBits, double speedFactor) {
+        super(speedBits, speedFactor, 0);
+        restrictions.addAll(Arrays.asList("foot", "access"));
+        restrictedValues.add("private");
+        restrictedValues.add("no");
+        restrictedValues.add("restricted");
+        restrictedValues.add("military");
+        restrictedValues.add("emergency");
+
+        intendedValues.add("yes");
+        intendedValues.add("designated");
+        intendedValues.add("official");
+        intendedValues.add("permissive");
+
+
+        setBlockByDefault(false);
+        potentialBarriers.add("gate");
+        potentialBarriers.add("stairs");
+
+        safeHighwayTags.add("footway");
+
+
+        //
+        allowedHighwayTags.addAll(safeHighwayTags);
+        allowedHighwayTags.addAll(avoidHighwayTags);
+
+
+        init();
+    }
+
+    @Override
+    public int getVersion() {
+        return 4;
+    }
+
+    @Override
+    public int defineWayBits(int index, int shift) {
+        // first two bits are reserved for route handling in superclass
+        shift = super.defineWayBits(index, shift);
+        // larger value required - ferries are faster than pedestrians
+        speedEncoder = new EncodedDoubleValue("Speed", shift, speedBits, speedFactor, MEAN_SPEED, maxPossibleSpeed);
+        shift += speedEncoder.getBits();
+
+        priorityWayEncoder = new EncodedValue("PreferWay", shift, 3, 1, 0, 7);
+        shift += priorityWayEncoder.getBits();
+        return shift;
+    }
+
+    @Override
+    public int defineRelationBits(int index, int shift) {
+        relationCodeEncoder = new EncodedValue("RelationCode", shift, 3, 1, 0, 7);
+        return shift + relationCodeEncoder.getBits();
+    }
+
+    /**
+     * Foot flag encoder does not provide any turn cost / restrictions
+     */
+    @Override
+    public int defineTurnBits(int index, int shift) {
+        return shift;
+    }
+
+    /**
+     * Foot flag encoder does not provide any turn cost / restrictions
+     * <p>
+     *
+     * @return <code>false</code>
+     */
+    @Override
+    public boolean isTurnRestricted(long flag) {
+        return false;
+    }
+
+    /**
+     * Foot flag encoder does not provide any turn cost / restrictions
+     * <p>
+     *
+     * @return 0
+     */
+    @Override
+    public double getTurnCost(long flag) {
+        return 0;
+    }
+
+    @Override
+    public long getTurnFlags(boolean restricted, double costs) {
+        return 0;
+    }
+
+    /**
+     * Some ways are okay but not separate for pedestrians.
+     * <p>
+     */
+    @Override
+    public long acceptWay(ReaderWay way) {
+        String highwayValue = way.getTag("highway");
+
+        // check access restrictions
+        if (way.hasTag(restrictions, restrictedValues) && !getConditionalTagInspector().isRestrictedWayConditionallyPermitted(way))
+            return 0;
+
+        if (!allowedHighwayTags.contains(highwayValue))
+            return 0;
+
+        if (getConditionalTagInspector().isPermittedWayConditionallyRestricted(way))
+            return 0;
+
+        return acceptBit;
+    }
+
+    @Override
+    // indoor ways aren't stored as relations
+    public long handleRelationTags(ReaderRelation relation, long oldRelationFlags) {
+        return 0;
+    }
+
+    @Override
+    public long handleWayTags(ReaderWay way, long allowed, long relationFlags) {
+        if (!isAccept(allowed))
+            return 0;
+
+        long flags = 0;
+
+        int priorityFromRelation = 0;
+        if (relationFlags != 0)
+            priorityFromRelation = (int) relationCodeEncoder.getValue(relationFlags);
+
+        flags = priorityWayEncoder.setValue(flags, handlePriority(way, priorityFromRelation));
+        return flags;
+    }
+
+    @Override
+    public double getDouble(long flags, int key) {
+        switch (key) {
+            case PriorityWeighting.KEY:
+                return (double) priorityWayEncoder.getValue(flags) / BEST.getValue();
+            default:
+                return super.getDouble(flags, key);
+        }
+    }
+
+    protected int handlePriority(ReaderWay way, int priorityFromRelation) {
+        TreeMap<Double, Integer> weightToPrioMap = new TreeMap<Double, Integer>();
+        if (priorityFromRelation == 0)
+            weightToPrioMap.put(0d, UNCHANGED.getValue());
+        else
+            weightToPrioMap.put(110d, priorityFromRelation);
+
+        collect(way, weightToPrioMap);
+
+        // pick priority with biggest order value
+        return weightToPrioMap.lastEntry().getValue();
+    }
+
+    /**
+     * @param weightToPrioMap associate a weight with every priority. This sorted map allows
+     *                        subclasses to 'insert' more important priorities as well as overwrite determined priorities.
+     */
+    void collect(ReaderWay way, TreeMap<Double, Integer> weightToPrioMap) {
+        String highway = way.getTag("highway");
+        if (way.hasTag("foot", "designated"))
+            weightToPrioMap.put(100d, PREFER.getValue());
+
+        double maxSpeed = getMaxSpeed(way);
+        if (safeHighwayTags.contains(highway) || maxSpeed > 0 && maxSpeed <= 20) {
+            weightToPrioMap.put(40d, PREFER.getValue());
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> feature) {
+        if (super.supports(feature))
+            return true;
+
+        return PriorityWeighting.class.isAssignableFrom(feature);
+    }
+
+    @Override
+    public String toString() {
+        return "indoor";
+    }
+
+
+    @Override
+    public void applyWayTags(ReaderWay way, EdgeIteratorState edge) {
+
+    }
+}
+
+
+
+
---
+++ core/src/main/java/com/graphhopper/search/LevelIndex.java
@@ -0,0 +1,151 @@
+/*
+ *  Licensed to GraphHopper GmbH under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for
+ *  additional information regarding copyright ownership.
+ *
+ *  GraphHopper GmbH licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.graphhopper.search;
+
+import com.graphhopper.storage.DataAccess;
+import com.graphhopper.storage.Directory;
+import com.graphhopper.storage.Storable;
+import com.graphhopper.util.BitUtil;
+import com.graphhopper.util.Helper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Ottavio Campana
+ * @author Peter Karich
+ */
+public class LevelIndex implements Storable<LevelIndex> {
+    private static final Logger logger = LoggerFactory.getLogger(LevelIndex.class);
+    private static final long START_POINTER = 1;
+    private final DataAccess levels;
+    private long bytePointer = START_POINTER;
+    // minor optimization for the previous stored name
+    private String lastLevel;
+    private long lastIndex;
+
+    public LevelIndex(Directory dir) {
+        levels = dir.find("levels");
+    }
+
+    @Override
+    public LevelIndex create(long initBytes) {
+        levels.create(initBytes);
+        return this;
+    }
+
+    @Override
+    public boolean loadExisting() {
+        if (levels.loadExisting()) {
+            bytePointer = BitUtil.LITTLE.combineIntsToLong(levels.getHeader(0), levels.getHeader(4));
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return the byte pointer to the level
+     */
+    public long put(String level) {
+        if (level == null || level.isEmpty()) {
+            return 0;
+        }
+        if (level.equals(lastLevel)) {
+            return lastIndex;
+        }
+        byte[] bytes = getBytes(level);
+        long oldPointer = bytePointer;
+        levels.ensureCapacity(bytePointer + 1 + bytes.length);
+        byte[] sizeBytes = new byte[]{
+                (byte) bytes.length
+        };
+        levels.setBytes(bytePointer, sizeBytes, sizeBytes.length);
+        bytePointer++;
+        levels.setBytes(bytePointer, bytes, bytes.length);
+        bytePointer += bytes.length;
+        lastLevel = level;
+        lastIndex = oldPointer;
+        return oldPointer;
+    }
+
+    private byte[] getBytes(String level) {
+        byte[] bytes = null;
+        for (int i = 0; i < 2; i++) {
+            bytes = level.getBytes(Helper.UTF_CS);
+            // we have to store the size of the array into *one* byte
+            if (bytes.length > 255) {
+                String newLevel = level.substring(0, 256 / 4);
+                logger.info("Way level is too long: " + level + " truncated to " + newLevel);
+                level = newLevel;
+                continue;
+            }
+            break;
+        }
+        if (bytes.length > 255) {
+            // really make sure no such problem exists
+            throw new IllegalStateException("Way level is too long: " + level);
+        }
+        return bytes;
+    }
+
+    public String get(long pointer) {
+        if (pointer < 0)
+            throw new IllegalStateException("Pointer to access LevelIndex cannot be negative:" + pointer);
+
+        // default
+        if (pointer == 0)
+            return "";
+
+        byte[] sizeBytes = new byte[1];
+        levels.getBytes(pointer, sizeBytes, 1);
+        int size = sizeBytes[0] & 0xFF;
+        byte[] bytes = new byte[size];
+        levels.getBytes(pointer + sizeBytes.length, bytes, size);
+        return new String(bytes, Helper.UTF_CS);
+    }
+
+    @Override
+    public void flush() {
+        levels.setHeader(0, BitUtil.LITTLE.getIntLow(bytePointer));
+        levels.setHeader(4, BitUtil.LITTLE.getIntHigh(bytePointer));
+        levels.flush();
+    }
+
+    @Override
+    public void close() {
+        levels.close();
+    }
+
+    @Override
+    public boolean isClosed() {
+        return levels.isClosed();
+    }
+
+    public void setSegmentSize(int segments) {
+        levels.setSegmentSize(segments);
+    }
+
+    @Override
+    public long getCapacity() {
+        return levels.getCapacity();
+    }
+
+    public void copyTo(LevelIndex nameIndex) {
+        levels.copyTo(nameIndex.levels);
+    }
+}
