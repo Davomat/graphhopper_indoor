3909cca0c68a869f6a1dadc4344c1b080612407a
Bettina Auschra <ausb@hrz.tu-chemnitz.de>
2018-07-01 18:11:01 +0200

    deleted redundant methods

--- core/src/main/java/com/graphhopper/routing/template/RoutingTemplateIndoor.java
+++ core/src/main/java/com/graphhopper/routing/template/RoutingTemplateIndoor.java
@@ -2,14 +2,10 @@ package com.graphhopper.routing.template;
 
 import com.graphhopper.GHRequest;
 import com.graphhopper.GHResponse;
-import com.graphhopper.GHRequestIndoor;
-import com.graphhopper.GHResponse;
-import com.graphhopper.PathWrapper;
-import com.graphhopper.routing.*;
+
 import com.graphhopper.routing.util.*;
 import com.graphhopper.storage.index.LocationIndex;
 import com.graphhopper.storage.index.QueryResult;
-import com.graphhopper.util.*;
 import com.graphhopper.util.exceptions.PointNotFoundException;
 import com.graphhopper.util.shapes.GHPoint;
 import com.graphhopper.util.shapes.GHPointIndoor;
@@ -19,17 +15,10 @@ import java.util.List;
 
 public class RoutingTemplateIndoor extends ViaRoutingTemplate implements RoutingTemplate {
 
-    protected final GHRequestIndoor ghRequest;
-    protected final GHResponse ghResponse;
-    protected final PathWrapper altResponse = new PathWrapper();
     private final LocationIndex locationIndex;
-    // result from route
-    protected List<Path> pathList;
 
     public RoutingTemplateIndoor(GHRequest ghRequest, GHResponse ghRsp, LocationIndex locationIndex) {
         super(ghRequest, ghRsp, locationIndex);
-        this.ghRequest = (GHRequestIndoor) ghRequest;
-        this.ghResponse = ghRsp;
         this.locationIndex = locationIndex;
     }
 
@@ -52,72 +41,6 @@ public class RoutingTemplateIndoor extends ViaRoutingTemplate implements Routing
         return queryResults;
     }
 
-    @Override
-    public List<Path> calcPaths(QueryGraph queryGraph, RoutingAlgorithmFactory algoFactory, AlgorithmOptions algoOpts) {
-        long visitedNodesSum = 0L;
-        int pointCounts = ghRequest.getPoints().size();
-        pathList = new ArrayList<>(pointCounts - 1);
-        QueryResult fromQResult = queryResults.get(0);
-        StopWatch sw;
-        for (int placeIndex = 1; placeIndex < pointCounts; placeIndex++) {
-            if (placeIndex == 1) {
-                // enforce start direction
-                queryGraph.enforceHeading(fromQResult.getClosestNode(), ghRequest.getFavoredHeading(0), false);
-            }
-
-            QueryResult toQResult = queryResults.get(placeIndex);
-
-            // enforce end direction
-            queryGraph.enforceHeading(toQResult.getClosestNode(), ghRequest.getFavoredHeading(placeIndex), true);
-
-            sw = new StopWatch().start();
-            RoutingAlgorithm algo = algoFactory.createAlgo(queryGraph, algoOpts);
-            String debug = ", algoInit:" + sw.stop().getSeconds() + "s";
-
-            sw = new StopWatch().start();
-            List<Path> tmpPathList = algo.calcPaths(fromQResult.getClosestNode(), toQResult.getClosestNode());
-            debug += ", " + algo.getName() + "-routing:" + sw.stop().getSeconds() + "s";
-            if (tmpPathList.isEmpty())
-                throw new IllegalStateException("At least one path has to be returned for " + fromQResult + " -> " + toQResult);
-
-            int idx = 0;
-            for (Path path : tmpPathList) {
-                if (path.getTime() < 0)
-                    throw new RuntimeException("Time was negative " + path.getTime() + " for index " + idx + ". Please report as bug and include:" + ghRequest);
-
-                pathList.add(path);
-                debug += ", " + path.getDebugInfo();
-                idx++;
-            }
-
-            altResponse.addDebugInfo(debug);
-
-            // reset all direction enforcements in queryGraph to avoid influencing next path
-            queryGraph.clearUnfavoredStatus();
-
-            if (algo.getVisitedNodes() >= algoOpts.getMaxVisitedNodes())
-                throw new IllegalArgumentException("No path found due to maximum nodes exceeded " + algoOpts.getMaxVisitedNodes());
-
-            visitedNodesSum += algo.getVisitedNodes();
-            fromQResult = toQResult;
-        }
-
-        ghResponse.getHints().put("visited_nodes.sum", visitedNodesSum);
-        ghResponse.getHints().put("visited_nodes.average", (float) visitedNodesSum / (pointCounts - 1));
-
-        return pathList;
-    }
-
-    @Override
-    public boolean isReady(PathMerger pathMerger, Translation tr) {
-        if (ghRequest.getPoints().size() - 1 != pathList.size())
-            throw new RuntimeException("There should be exactly one more points than paths. points:" + ghRequest.getPoints().size() + ", paths:" + pathList.size());
-
-        altResponse.setWaypoints(getWaypoints());
-        ghResponse.add(altResponse);
-        pathMerger.doWork(altResponse, pathList, tr);
-        return true;
-    }
 
 }
 
